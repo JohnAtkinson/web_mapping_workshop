9159e947b99a43c2b4201a8ead13b7f2a7848996
JohnAtkinson <john.m.atkinson@usace.army.mil>
2014-11-13 14:28:47 -0600

    Update app_2.js

--- js/app_2.js
+++ js/app_2.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'johnatkinson.k7c253n7'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1Ijoiam9obmF0a2luc29uIiwiYSI6InVLM280SGsifQ.IBOj-xq9MTl9_sT_REsSTA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
