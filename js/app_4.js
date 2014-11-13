00564951cdc4b3244ae35f84fc94f26ef51f2264
JohnAtkinson <john.m.atkinson@usace.army.mil>
2014-11-13 15:08:56 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'johnatkinson.k7c253n7'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1Ijoiam9obmF0a2luc29uIiwiYSI6InVLM280SGsifQ.IBOj-xq9MTl9_sT_REsSTA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/powercat.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/bike_map.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
@@ -25,7 +25,7 @@ featureLayer.on('ready', function() {
     this.setStyle({
         "color": "#43094c",
         "fillColor": "#43094c",
-        "weight": .5,
+        "weight": 3,
         "opacity": 0.65
     });
     map.fitBounds(featureLayer.getBounds());
